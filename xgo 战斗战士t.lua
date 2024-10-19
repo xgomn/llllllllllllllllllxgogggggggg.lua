dae7e1d9ea9fe1db929e5692008f88741494ac30
xgomn <3641643802@qq.com>
2024-10-19 20:32:01 +0800

    Update xgo 战斗战士t.lua

--- xgo 战斗战士t.lua
+++ xgo 战斗战士t.lua
@@ -1,18 +1,1 @@
---你在哪里
-local ui = loadstring(game:HttpGet("https://raw.githubusercontent.com/GTAFAW/neilonmacedo153/refs/heads/main/xgoui.txt"))();    
-local win = ui:new("goto战斗战士")
 
-local UITab1 = win:Tab("『信息』",'6035145364')
-
-local about = UITab1:section("『信息』",true)
-
-
-about:Label("作者：goto_O")
-about:Label("goto战斗战士")
-about:Label("你的注入器:"..identifyexecutor())
-about:Label("你的用户名:"..game.Players.LocalPlayer.Character.Name)
-
-local UITab2 = win:Tab("『主要』",'16060333448')
-
-local about = UITab2:section("『xxxxxxxgo』",true)   
- 
